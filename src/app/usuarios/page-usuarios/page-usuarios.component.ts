9664224bc1aa5dfdf8d1ac03b153bda825510967
borilio <exprimetuandroid@gmail.com>
2022-06-15 09:23:32 +0200

    Terminado con Formulario Reactivos. Justo antes de empezar a añadir un login

--- src/app/usuarios/page-usuarios/page-usuarios.component.ts
+++ src/app/usuarios/page-usuarios/page-usuarios.component.ts
@@ -4,6 +4,7 @@ import {UsuariosService} from "../../shared/services/usuarios.service";
 import {ConfirmationService, MessageService, PrimeIcons} from "primeng/api";
 import {HttpErrorResponse} from "@angular/common/http";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ValidadorService} from "../../shared/services/validador.service";
 
 @Component({
   selector: 'app-page-usuarios',
@@ -24,6 +25,7 @@ export class PageUsuariosComponent implements OnInit {
   public procesoGuardando: boolean;
 
   //Ventana de diálogo Formulario Usuario
+  public estoyModificando: boolean;
   public mostrandoDialogoUsuario: boolean;
 
   //Formulario Reactivo Usuario
@@ -34,12 +36,14 @@ export class PageUsuariosComponent implements OnInit {
     private _usuariosService: UsuariosService,
     private _mensajesService: MessageService,
     private _confirmarService: ConfirmationService,
-    private _formBuilder : FormBuilder
+    private _formBuilder : FormBuilder,
+    private _validacionService: ValidadorService
   ) {
     this.procesoCargaListaUsuarios = false;
     this.procesoBorrando = false;
     this.procesoGuardando = false;
     this.mostrandoDialogoUsuario = false;
+    this.estoyModificando = false;
 
     this.usuario = <Usuario>{};
     this.rolesDisponibles = [
@@ -52,7 +56,7 @@ export class PageUsuariosComponent implements OnInit {
     const regexClave = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'; //al menos 8 dígitos, 1 mayúscula, 1 minúscula y un número y permite carácteres especiales
     this.formUsuario = this._formBuilder.group({
       id: [ null, [Validators.required, Validators.min(1)]],
-      nombre : ['', [Validators.required, Validators.minLength(3)]],
+      nombre : ['', [Validators.required, this._validacionService.validacionNombre ]],
       correo: ['', [Validators.required, Validators.email]],
       clave: ['', [Validators.required, Validators.pattern(regexClave)]],
       rol: [this.rolesDisponibles[1]]
@@ -130,10 +134,16 @@ export class PageUsuariosComponent implements OnInit {
 
   }
 
-  public mostrarDialogoFormularioUsuario(){
-    this.formUsuario.reset({
-      rol : this.rolesDisponibles[1]
-    }); // Pone todos los valores por defecto
+  public mostrarDialogoFormularioUsuario(editar?:boolean){
+    //Si es un usuario nuevo, reseteo, si no, no hago nada
+    if (!editar) {
+      this.estoyModificando = false;
+      this.formUsuario.reset({
+        rol: this.rolesDisponibles[1]
+      }); // Pone todos los valores por defecto
+    } else {
+      this.estoyModificando = true;
+    }
     this.mostrandoDialogoUsuario = true;
   }
 
@@ -143,35 +153,67 @@ export class PageUsuariosComponent implements OnInit {
 
   public guardarUsuario(usuario: Usuario){
     this.procesoGuardando = true;
-    this._usuariosService.add(usuario).subscribe({
-      next: (resp) => {
-        this._mensajesService.add({
-          severity: 'success',
-          summary: 'Usuario añadido',
-          detail: `Se añadió correctamente el usuario "${resp.nombre}"`,
-          life: 2000
-        });
-        this.procesoGuardando = false;
-        this.ocultarDialogoFormularioUsuario();
-        this.cargarDatos();
-      },
-      error: (error: HttpErrorResponse) => {
-        this._mensajesService.add({
-          severity: 'error',
-          summary: "Error",
-          detail: error.message
-        });
-        this.procesoGuardando = false;
-        //Podríamos cerrar la ventana de diálogo, pero mejor se la dejamos abierta y que
-        //vuelva a intentar la petición si quiere, o que la cierre si no.
-      }
-    });
-    //TODO: Cuando hagamos una modificación, deberemos llamar a .modify() en lugar de .add()
+    //Si pulsó el lápiz de editar... estamos editando, y si no es que queremos guardar uno nuevo
+    if (this.estoyModificando) {
+      this._usuariosService.modify(usuario).subscribe({
+        next: (resp) => {
+          this._mensajesService.add({
+            severity: 'success',
+            summary: 'Usuario modificado',
+            detail: `Se modificó correctamente el usuario "${resp.nombre}"`,
+            life: 2000
+          });
+          this.procesoGuardando = false;
+          this.ocultarDialogoFormularioUsuario();
+          this.cargarDatos();
+          this.estoyModificando = false;
+        },
+        error: (error: HttpErrorResponse) => {
+          this._mensajesService.add({
+            severity: 'error',
+            summary: "Error",
+            detail: error.message
+          });
+          this.procesoGuardando = false;
+          this.estoyModificando = false;
+          //Podríamos cerrar la ventana de diálogo, pero mejor se la dejamos abierta y que
+          //vuelva a intentar la petición si quiere, o que la cierre si no.
+        }
+      });
+    } else {
+      this._usuariosService.add(usuario).subscribe({
+        next: (resp) => {
+          this._mensajesService.add({
+            severity: 'success',
+            summary: 'Usuario añadido',
+            detail: `Se añadió correctamente el usuario "${resp.nombre}"`,
+            life: 2000
+          });
+          this.procesoGuardando = false;
+          this.ocultarDialogoFormularioUsuario();
+          this.cargarDatos();
+        },
+        error: (error: HttpErrorResponse) => {
+          this._mensajesService.add({
+            severity: 'error',
+            summary: "Error",
+            detail: error.message
+          });
+          this.procesoGuardando = false;
+          //Podríamos cerrar la ventana de diálogo, pero mejor se la dejamos abierta y que
+          //vuelva a intentar la petición si quiere, o que la cierre si no.
+        }
+      });
+    }
   }
 
   public editarUsuario(usuario: Usuario) {
-    this.usuario = usuario;
-    this.mostrarDialogoFormularioUsuario();
+    //Como el objeto usuario tiene una propiedad (admin) que no está definida en el formulario, podemos:
+    //-Definirla en el formulario
+    //-Borrarla en el objeto (para borrar una propiedad, ésta debe declararse como opcional en su interfaz con '?'
+    delete usuario.admin;
+    this.formUsuario.setValue(usuario); //Volcamos el objeto Usuario que queremos modificar en el formulario
+    this.mostrarDialogoFormularioUsuario(true);
   }
 
   public formularioUsuarioEnviado() {

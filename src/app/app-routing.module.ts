900639e85450a2396cc89ee7e634728bb52dded1
borilio <exprimetuandroid@gmail.com>
2022-06-15 10:03:09 +0200

    Creado el módulo de Auth, con sus componentes, sus rutas hijas y su módulo de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import {PageUsuariosComponent} from "./usuarios/page-usuarios/page-usuarios.component";
 import {PageIncidenciasComponent} from "./incidencias/page-incidencias/page-incidencias.component";
 import {PageCentralesComponent} from "./centrales/page-centrales/page-centrales.component";
+import {LoginComponent} from "./auth/pages/login/login.component";
+import {RegistroComponent} from "./auth/pages/registro/registro.component";
 
 const routes: Routes = [
   {path: "", component: PageUsuariosComponent, pathMatch: "full" },
   {path: "usuarios", component: PageUsuariosComponent},
   {path: "incidencias", component: PageIncidenciasComponent},
   {path: "centrales", component: PageCentralesComponent },
+  {path: "auth/login", component: LoginComponent},
+  {path: "auth/registro", component: RegistroComponent},
   {path: "**", redirectTo: ""}
 ];
 

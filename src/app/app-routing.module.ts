2a7a79665a1a19eb5136334895437d7df62c97f9
borilio <exprimetuandroid@gmail.com>
2022-06-21 09:43:47 +0200

    Cambios menores en el routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import {PageUsuariosComponent} from "./usuarios/page-usuarios/page-usuarios.component";
 import {PageIncidenciasComponent} from "./incidencias/page-incidencias/page-incidencias.component";
 import {PageCentralesComponent} from "./centrales/page-centrales/page-centrales.component";
+import {AuthGuard} from "./auth/guards/auth.guard";
 
 const routes: Routes = [
 
   {path: "", redirectTo: "auth/login", pathMatch: "full"},
 
-  { path: "auth", loadChildren: ()=> import('./auth/auth.module').then( m => m.AuthModule) },
-  { path: "usuarios", loadChildren: ()=> import('./usuarios/usuarios.module').then( m => m.UsuariosModule)},
+  {
+    path: "auth",
+    loadChildren: ()=> import('./auth/auth.module').then( m => m.AuthModule)
+  },
+  {
+    path: "usuarios",
+    loadChildren: ()=> import('./usuarios/usuarios.module').then( m => m.UsuariosModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard]
+  },
 
   {path: "incidencias", component: PageIncidenciasComponent},
   {path: "centrales", component: PageCentralesComponent },
